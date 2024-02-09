1ae93d5ee850ad2c053922c9f9d16d361e2cbdbc
Tyler Fields <300032214@fvtc.edu>
2024-02-08 19:55:39 -0600

    Added BL / BL.Models / UI

---
+++ BookKeepers.BL.Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookKeepers.BL.Models
+{
+    internal class Comment
+    {
+    }
+}
