751dee3f4b8b90b0895c7130ebaa0c887329eb79
Isaiah LaValley <300027888@fvtc.edu>
2024-02-27 21:33:46 -0600

    added all the stuff for user including the table itself

--- BookKeepers.DB/PostDeploy/DefaultData/Users.sql
+++ BookKeepers.DB/PostDeploy/DefaultData/Users.sql
@@ -1,1 +1,7 @@
-﻿
+﻿BEGIN
+	INSERT INTO tblUser (Id,FirstName,LastName,Password)
+	VALUES
+	(1,'James','Potter','whyArey0uGay'),
+	(2,'Jim','Bob','ThatsNotIT'),
+	(3,'Becky','Green','WhyIsHeHere')
+END
--- BookKeepers.DB/Tables/tblUser.sql
+++ BookKeepers.DB/Tables/tblUser.sql
@@ -1,4 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblUser]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NULL, 
+    [LastName] VARCHAR(50) NULL, 
+    [Password] VARCHAR(50) NULL
 )
