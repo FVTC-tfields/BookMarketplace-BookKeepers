31c1b7c103a0fb326ea81a85e60a0e4ecb71211e
Isaiah LaValley <300027888@fvtc.edu>
2024-03-26 04:32:45 -0500

    fixed some things and tidyed up a bit :)

--- BookKeepers.BL.Models/Book.cs
+++ BookKeepers.BL.Models/Book.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +24,5 @@ namespace BookKeepers.BL.Models
         public string? PublisherName { get; set; }
         [DisplayName("Subject")]
         public string? SubjectName { get; set; }
-        
     }
 }
